1250384fdab5d261cf877f68791c75ab1d2edc22
Beginner <ryuton6c@gmail.com>
2020-07-10 15:04:41 +0900

    Profile追加
    そのうち頑張ります

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/picture1.JPG') {
+        myImage.setAttribute ('src', 'images/picture2.JPG');
+    } else {
+        myImage.setAttribute ('src', 'images/picture1.JPG');
+    }
+}
+/*
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if (!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+}
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function () {
+    setUserName();
+}
+*/
